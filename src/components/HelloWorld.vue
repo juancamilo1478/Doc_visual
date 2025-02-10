ee955e8337ee04855f0dc621830140a3a5a00779
juanc <juancamilo1478@gmail.com>
2025-02-10 13:25:24 -0500

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+ <h1>hello Word</h1>
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+<h1 class="text-amber-200">hello word</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Home",
+});
+
+</script>
