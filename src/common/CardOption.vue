ced62c3011f152405172a1208a77fd932461aa4a
juanc <juancamilo1478@gmail.com>
2025-02-11 11:15:07 -0500

    save

--- src/common/CardOption.vue
+++ src/common/CardOption.vue
@@ -13,13 +13,13 @@ defineProps<{
             }}</a>
 
         </div>
-        <div class="w-full px-6 pb-5">
-            <img class="object-contain w-full h-56" :src="image" alt="Imagen de opción">
-            <button
-                class="w-full px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform  rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" style="background: var(--blue-1);">
+        <div class="flex flex-col items-center px-6 pb-4">
+            <img class="object-cover w-auto h-56 max-w-full" :src="image" alt="Imagen de opción">
+            <button class="w-full   py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" style="background: var(--blue-1);">
                 Pedir cita ahora
             </button>
         </div>
+        
         
     </div>
 
