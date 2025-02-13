221b7d99f0c6959c5843f340dc3c17d9a5d3416f
juanc <juancamilo1478@gmail.com>
2025-02-12 20:39:19 -0500

    login

--- src/Modules/Home/Bulletin.vue
+++ src/Modules/Home/Bulletin.vue
@@ -11,14 +11,14 @@ export default {
 
 <template>
     <div class="w-screen flex flex-col md:flex-row">
-        <div class="w-1/2 h-96 bg-blue-500">
+        <div class="w-1/2 h-96  md:w-full bg-blue-500 ">
             <img 
             src="@/assets/images/optica1.jpg" 
             alt="Imagen 1" 
              class="w-full h-full object-cover"
           />
         </div>
-        <div class="w-1/2 h-96  ">
+        <div class="w-1/2 h-96  md:w-full ">
             <BulletingForm />
         </div>
       </div>
