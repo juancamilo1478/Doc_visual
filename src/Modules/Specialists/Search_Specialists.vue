ac1fe5b445c208817697549dad6a07aff1cadf7b
juanc <juancamilo1478@gmail.com>
2025-02-19 22:45:06 -0500

    save

--- src/Modules/Specialists/Search_Specialists.vue
+++ src/Modules/Specialists/Search_Specialists.vue
@@ -48,7 +48,16 @@ export default {
         },
         searchItems() {
             let itemsFull = [...this.items]; // Clonamos para evitar mutaciones inesperadas
+            const normalizeText = (text: string) =>
+                text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
 
+            if (this.name.trim().length > 0) {
+                const normalizedSearch = normalizeText(this.name.trim());
+
+                itemsFull = itemsFull.filter(item =>
+                    normalizeText(item.name).includes(normalizedSearch)
+                );
+            }
             if (this.filterSpecialty.length > 0) {
                 itemsFull = itemsFull.filter(item =>
                     item.specialist.some(specialty => this.filterSpecialty.includes(specialty))
@@ -78,7 +87,8 @@ export default {
         <div class="container m-auto px-2 mt-10">
             <h1 class="w-full text-center font-poppins text-2xl" style="color: var(--blue-1);">Especialistas en salud
                 visual a la mano</h1>
-            <h2 class="w-full text-center font-poppins mt-2 mb-9 text-3xl font-bold">Agenda tu sesion con alguno de nuestros
+            <h2 class="w-full text-center font-poppins mt-2 mb-9 text-3xl font-bold">Agenda tu sesion con alguno de
+                nuestros
                 especialistas en</h2>
             <div class="flex flex-wrap justify-center gap-20 my-10">
 
@@ -112,6 +122,24 @@ export default {
                 <div class="w-3/10 mx-9 bg-white flex flex-col border h-full sticky top-10">
                     <!-- Contenido con scroll -->
                     <div class="overflow-y-auto p-3 max-h-[50vh] min-h-0">
+
+                        <div class="w-full">
+                            <h1 class="mb-2 font-poppins mx-2 mt-4">Buscar por nombre</h1>
+                            <div class="relative flex items-center w-full   ">
+
+                                <svg class="absolute left-3 w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+                                        stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </svg>
+                                <input type="text" placeholder="Buscar..." v-model="name"
+                                    class="w-full pl-10 pr-3 py-2 mx-2  rounded-full    border border-gray-400 focus:ring-2 focus:ring-blue-500 font-poppins my-10" />
+                            </div>
+                        </div>
+
+
                         <div class="w-full ">
                             <div class="w-full flex items-center px-2" @click="setPanel('panelService')">
                                 <h1 class="text-xl font-medium font-poppins ">Servicio</h1>
@@ -129,7 +157,7 @@ export default {
                                             :checked="isSpecialistSelected(option)" :value="option"
                                             class="w-5 h-5 mx-2 colorvar pointer-events-none">
                                         <h1 class="mt-auto cursor-pointer" @click="selectSpecialist(option)">{{ option
-                                        }}</h1>
+                                            }}</h1>
                                     </div>
                                 </div>
                             </Transition>
@@ -152,7 +180,7 @@ export default {
                                             :checked="isSpecialistSelected(option)" :value="option"
                                             class="w-5 h-5 mx-2 colorvar pointer-events-none">
                                         <h1 class="mt-auto cursor-pointer" @click="selectSpecialist(option)">{{ option
-                                        }}</h1>
+                                            }}</h1>
                                     </div>
                                 </div>
                             </Transition>
@@ -175,7 +203,7 @@ export default {
                                             :checked="isSpecialistSelected(option)" :value="option"
                                             class="w-5 h-5 mx-2 colorvar pointer-events-none">
                                         <h1 class="mt-auto cursor-pointer" @click="selectSpecialist(option)">{{ option
-                                        }}</h1>
+                                            }}</h1>
                                     </div>
                                 </div>
                             </Transition>
@@ -196,7 +224,8 @@ export default {
                 </div>
 
                 <div class="w-7/10  ">
-                    <h1 v-if="data.length" class="font-poppins mx-2">{{ `${data.length} Especialistas disponibles para ti` }}</h1>
+                    <h1 v-if="data.length" class="font-poppins mx-2">{{ `${data.length} Especialistas disponibles para
+                        ti` }}</h1>
                     <div class="flex flex-wrap justify-center gap-4 p-4">
                         <div v-for="(option, index) in data" :key="index"
                             class="flex flex-col items-center   p-4 rounded-lg">
