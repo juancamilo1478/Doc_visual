f58918943eda9ae7effb723271b95531842ad52b
juanc <juancamilo1478@gmail.com>
2025-03-03 02:38:39 -0500

    save

--- src/Modules/Specialists/SpecialistDetail.vue
+++ src/Modules/Specialists/SpecialistDetail.vue
@@ -65,8 +65,16 @@ export default {
 
     computed: {
         monthName(): string | null {
-            return this.exploreMonth !== null ? getMonthName(this.exploreMonth) : null;
+            if (this.listDates.length === 0) {
+                return null;
+            }
+            const firstMonth = this.listDates[0].month;
+            const lastMonth = this.listDates[this.listDates.length - 1].month;
+            const firstMonthName = getMonthName(firstMonth);
+            const lastMonthName = getMonthName(lastMonth);
+            return firstMonth === lastMonth ? firstMonthName : `${firstMonthName} - ${lastMonthName}`;
         }
+       
     },
 
     mounted() {
@@ -378,9 +386,9 @@ export default {
                         <div class="relative w-[90%] m-auto  mb-3">
                             <!-- Ícono -->
 
-                              <!-- Ícono -->
-                              <img src="@/assets/images/iconconsultorio.png"
-                              class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
+                            <!-- Ícono -->
+                            <img src="@/assets/images/iconconsultorio.png"
+                                class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
 
                             <!-- Select -->
                             <select v-model="localselect" id="specialist"
@@ -399,7 +407,7 @@ export default {
 
 
                             <img src="@/assets/images/icopnprepagada.png"
-                            class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
+                                class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
                             <!-- Select -->
                             <select v-model="selectpay" id="specialist"
                                 class="border p-2 pl-10 w-full rounded-xl flex appearance-none border-gray-400">
@@ -419,7 +427,7 @@ export default {
 
 
 
-                        <h1 class="mx-8 font-semibold text-base"> {{ monthName }}{{ currentYear }}</h1>
+                        <h1 class="mx-8 font-semibold text-base"> {{ monthName }}-{{ currentYear }}</h1>
                         <div class="flex   justify-between p-4  px-8"
                             v-if="listDates.length && currentDay != null && currentMonth != null && currentYear != null">
                             <!-- Flecha izquierda -->
@@ -562,7 +570,8 @@ export default {
                                 :href="specialist?.social.instagram" target="_blank" rel="noopener noreferrer">
                                 <div
                                     class="w-5 h-5 md:w-8 md:h-8 bg-[var(--grayicon-1)] rounded-full flex items-center justify-center  hover:bg-[var(--blue-1)] transition shadow-lg ">
-                                    <svg class="  w-3 h-3 md:w-5 md:h-5" fill="#ffffff" viewBox="0 0 24 24" aria-hidden="true">
+                                    <svg class="  w-3 h-3 md:w-5 md:h-5" fill="#ffffff" viewBox="0 0 24 24"
+                                        aria-hidden="true">
                                         <path fill-rule="evenodd"
                                             d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
                                             clip-rule="evenodd"></path>
@@ -577,8 +586,8 @@ export default {
                                 :href="specialist?.social.linkedin" target="_blank" rel="noopener noreferrer">
                                 <div
                                     class="w-5 h-5 md:w-8 md:h-8 bg-[var(--grayicon-1)] rounded-full flex items-center justify-center  hover:bg-[var(--blue-1)] shadow-lg transition">
-                                    <svg class="w-3 h-3 md:w-5 md:h-5" fill="#ffffff" width="800px" height="800px" viewBox="0 0 32 32"
-                                        version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                    <svg class="w-3 h-3 md:w-5 md:h-5" fill="#ffffff" width="800px" height="800px"
+                                        viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                         <title>linkedin</title>
                                         <path
                                             d="M28.778 1.004h-25.56c-0.008-0-0.017-0-0.027-0-1.199 0-2.172 0.964-2.186 2.159v25.672c0.014 1.196 0.987 2.161 2.186 2.161 0.010 0 0.019-0 0.029-0h25.555c0.008 0 0.018 0 0.028 0 1.2 0 2.175-0.963 2.194-2.159l0-0.002v-25.67c-0.019-1.197-0.994-2.161-2.195-2.161-0.010 0-0.019 0-0.029 0h0.001zM9.9 26.562h-4.454v-14.311h4.454zM7.674 10.293c-1.425 0-2.579-1.155-2.579-2.579s1.155-2.579 2.579-2.579c1.424 0 2.579 1.154 2.579 2.578v0c0 0.001 0 0.002 0 0.004 0 1.423-1.154 2.577-2.577 2.577-0.001 0-0.002 0-0.003 0h0zM26.556 26.562h-4.441v-6.959c0-1.66-0.034-3.795-2.314-3.795-2.316 0-2.669 1.806-2.669 3.673v7.082h-4.441v-14.311h4.266v1.951h0.058c0.828-1.395 2.326-2.315 4.039-2.315 0.061 0 0.121 0.001 0.181 0.003l-0.009-0c4.5 0 5.332 2.962 5.332 6.817v7.855z">
@@ -595,7 +604,8 @@ export default {
                                 :href="specialist?.social.youtube" target="_blank" rel="noopener noreferrer">
                                 <div
                                     class="w-5 h-5 md:w-8 md:h-8 bg-[var(--grayicon-1)] rounded-full flex items-center justify-center shadow-lg hover:bg-[var(--blue-1)] transition">
-                                    <svg class="w-3 h-3 md:w-5 md:h-5" fill="#ffffff" viewBox="0 0 24 24" aria-hidden="true">
+                                    <svg class="w-3 h-3 md:w-5 md:h-5" fill="#ffffff" viewBox="0 0 24 24"
+                                        aria-hidden="true">
                                         <path fill-rule="evenodd"
                                             d="M10 15.5l6-3.5-6-3.5v7zM22 12s0 4.08-.69 6.02a3.27 3.27 0 01-2.31 2.31C17.08 21 12 21 12 21s-5.08 0-6.98-.67a3.27 3.27 0 01-2.31-2.31C2 16.08 2 12 2 12s0-4.08.69-6.02a3.27 3.27 0 012.31-2.31C6.92 3 12 3 12 3s5.08 0 6.98.67a3.27 3.27 0 012.31 2.31C22 7.92 22 12 22 12z"
                                             clip-rule="evenodd">
@@ -669,19 +679,20 @@ export default {
 
                         </div>
                     </div>
-                    <div class="w-full rounded-2xl border mt-3 border-gray-300 bg-white">
+                    <div class="text-xs md:text-base w-full rounded-2xl border mt-3 border-gray-300 bg-white">
                         <div class="w-full p-8 font-poppins text-base">
                             <h1 class="font-bold">Campos de acción</h1>
-                            <ul class="list-disc pl-5">
-                                <li v-for="(item, index) in specialist?.specialist" :key="index" class="text-sm">
+                            <ul class="list-disc pl-5 mt-5">
+                                <li v-for="(item, index) in specialist?.specialist" :key="index"
+                                    class="text-xs md:text-base">
                                     {{ item }}
                                 </li>
                             </ul>
 
                         </div>
                     </div>
-                    <div class="w-full rounded-2xl border mt-3 border-gray-300 bg-white">
-                        <div class="w-full p-8 font-poppins text-base">
+                    <div class="text-xs md:text-base w-full rounded-2xl border mt-3 border-gray-300 bg-white">
+                        <div class="w-full p-8 font-poppins ">
                             <h1 class="font-bold">Página web</h1>
                             <p class="mt-4 text-sm">
                                 {{ specialist?.web }}
@@ -689,9 +700,9 @@ export default {
 
                         </div>
                     </div>
-                    <div class="w-full rounded-2xl border mt-3 border-gray-300 bg-white">
-                        <div class="w-full p-8 font-poppins text-base">
-                            <h1 class="font-bold">Consultorio</h1>
+                    <div class="ext-xs md:text-base w-full rounded-2xl border mt-3 border-gray-300 bg-white">
+                        <div class="w-full p-8 font-poppins  ">
+                            <h1 class="font-bold ">Consultorio</h1>
                             <p>{{ localselect?.name }}</p>
                             <p class="mt-4 text-sm">
                                 {{ localselect?.direction }}
@@ -723,11 +734,11 @@ export default {
                     </div>
 
 
-                    <div class="w-full rounded-2xl border mt-3 border-gray-300 bg-white">
-                        <div class="w-full p-8 font-poppins text-base">
+                    <div class="text-xs md:text-base w-full rounded-2xl border mt-3 border-gray-300 bg-white">
+                        <div class="w-full p-8 font-poppins  ">
                             <h1 class="font-bold mb-6">Servicios y precios</h1>
                             <div v-for="(data, index) in specialist?.servicesCost" :key="index"
-                                class="font-poppins text-sm font-mono">
+                                class="font-poppins   font-mono">
                                 <p>{{ data.nameService }}</p>
                                 <p class="mb-2 mt-2">${{ data.price }}</p>
                                 <hr class="text-gray-300" />
@@ -979,9 +990,9 @@ export default {
                         <div class="relative w-[90%] m-auto  mb-3">
                             <!-- Ícono -->
 
-                              <!-- Ícono -->
-                              <img src="@/assets/images/iconconsultorio.png"
-                              class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
+                            <!-- Ícono -->
+                            <img src="@/assets/images/iconconsultorio.png"
+                                class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
 
                             <!-- Select -->
                             <select v-model="localselect" id="specialist"
@@ -1000,7 +1011,7 @@ export default {
 
 
                             <img src="@/assets/images/icopnprepagada.png"
-                            class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
+                                class="absolute left-3 top-1/2 transform -translate-y-1/2 w-8 h-8 p-1" alt="Icono">
                             <!-- Select -->
                             <select v-model="selectpay" id="specialist"
                                 class="border p-2 pl-10 w-full rounded-xl flex appearance-none border-gray-400">
@@ -1011,21 +1022,33 @@ export default {
                             </select>
                         </div>
 
-
-                        <h1 class="mx-8 font-semibold text-base"> {{ monthName }}{{ currentYear }}</h1>
-                        <div class="flex   justify-between p-4  px-8"
+                        <h1 class="mx-4 mb-3  font-semibold text-base"> {{ monthName }}-{{ currentYear }}</h1>
+                       
+                        <div class="   relative     "
                             v-if="listDates.length && currentDay != null && currentMonth != null && currentYear != null">
-                            <!-- Flecha izquierda -->
-                            <button @click="getListDatesBackwards"
-                                class="w-5 h-12 mt-3 flex items-center justify-center text-gray-600 hover:text-gray-900  cursor-pointer"
-                                v-if="!isToday(listDates[0].day, listDates[0].month, listDates[0].year, currentDay, currentMonth, currentYear)">
-                                <img src="@/assets/svg/arrow.svg" alt="Icono"
-                                    class="w-4 h-4 transition-transform ml-auto rotate-180">
-                            </button>
+                        
 
+                            <div class="absolute top-0 left-0 w-full flex justify-between  ">
+                                <!-- Flecha izquierda -->
+                                <button @click="getListDatesBackwards" class="w-12 h-12 mt-2 flex items-center justify-center  text-gray-600 hover:text-gray-900 cursor-pointer 
+                        transition-opacity duration-300" :class="isToday(listDates[0].day, listDates[0].month, listDates[0].year, currentDay, currentMonth, currentYear)
+                            ? 'opacity-0 invisible'
+                            : 'opacity-100 visible'">
+                            <img src="@/assets/svg/arrow.svg" alt="Icono"
+                            class="w-4 h-4 transition-transform   rotate-180">
+                                </button>
+
+                                <!-- Flecha derecha -->
+                                <button
+                                      class="w-12 h-12 mt-2 flex items-center justify-center text-gray-600 hover:text-gray-900 cursor-pointer"
+                                    @click="nextdate">
+                                    <img src="@/assets/svg/arrow.svg" alt="Icono"
+                                        class="w-4 h-4 transition-transform  ">
+                                </button>
+                            </div>
                             <!-- Grid de 3 columnas -->
-                            <div class="w-full grid grid-cols-3 gap-4 ">
-                                <div class="  text-black p-4 rounded-lg text-center">
+                            <div class="w-full flex justify-between ">
+                                <div class="w-[30%]  text-black p-4 rounded-lg text-center">
                                     <h1 class="text-[var(--blue-1)] pb-2 font-semibold"
                                         v-if="!isToday(listDates[0].day, listDates[0].month, listDates[0].year, currentDay, currentMonth, currentYear)">
                                         {{ getDayNameSpanish(listDates[0].month, listDates[0].day, listDates[0].year) }}
@@ -1046,7 +1069,7 @@ export default {
                                     </div>
                                 </div>
 
-                                <div class="  text-black p-4 rounded-lg text-center">
+                                <div class="w-[30%]  text-black p-4 rounded-lg text-center">
                                     <h1 class="  pb-2 font-semibold"
                                         :class="{ 'text-[var(--blue-1)]': listDates[1].isValid, 'text-[var(--blue-1)]/50': !listDates[1].isValid }">
                                         {{
@@ -1064,7 +1087,7 @@ export default {
                                         <h1>{{ getTimeString(data.hour, data.minute) }}</h1>
                                     </div>
                                 </div>
-                                <div class="  text-black p-4 rounded-lg text-center">
+                                <div class=" w-[30%] text-black p-4 rounded-lg text-center">
                                     <h1 class="  pb-2 font-semibold"
                                         :class="{ 'text-[var(--blue-1)]': listDates[2].isValid, 'text-[var(--blue-1)]/50': !listDates[2].isValid }">
                                         {{
@@ -1083,13 +1106,7 @@ export default {
                                 </div>
                             </div>
 
-                            <!-- Flecha derecha -->
-                            <button
-                                class="w-5 h-12 mt-3 flex items-center justify-center text-gray-600 hover:text-gray-900 cursor-pointer "
-                                @click="nextdate">
-                                <img src="@/assets/svg/arrow.svg" alt="Icono"
-                                    class="w-4 h-4 transition-transform ml-auto">
-                            </button>
+
                         </div>
                         <h1 class="text-center font-bold text-[var(--blue-1)]">ver más horarios</h1>
                         <div class="w-full">
